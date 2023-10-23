baddbd98e45ba98adf139771d8773a1bf24c5e74
Yazdan <yazdan.ghezelbash@gmail.com>
2023-10-23 11:02:18 +0330

    the cleancode

--- sallary2.py
+++ sallary2.py
@@ -11,6 +11,6 @@ def over_sallary(over_time):
  return (160*rate)+(over_time*rate*1.5)
 #____________________________  
 if hour<=160 :
-  print (normal_sallary())
+  print ("Your sallary is : ",normal_sallary())
 else:
- print(over_sallary(over_time()))
+ print("Your sallary is : ",over_sallary(over_time()))
